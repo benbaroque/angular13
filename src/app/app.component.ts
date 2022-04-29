043d7c8c9ac51ebaab8c8ba19eefa44b96107a24
bhattarayudh.p <bhattarayudh.p@kbtg.tech>
2022-04-29 10:58:54 +0700

    properties and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  name = '';
+
+  doSomeThing(event: Event){
+    let val = (event.target as HTMLInputElement).value;
+    this.name = val;
+  }
 }
