23b9152aec8687b748fb030dc6a302de32e87be4
bhattarayudh.p <bhattarayudh.p@kbtg.tech>
2022-04-29 18:35:49 +0700

    deploy hw

--- src/app/newpipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NewpipePipe } from './newpipe.pipe';
-
-describe('NewpipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new NewpipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
